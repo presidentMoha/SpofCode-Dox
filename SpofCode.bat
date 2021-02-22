59fe5f32b89ccbf158a1a9ff50d76baafd358f29
presidentMoha <79421379+presidentMoha@users.noreply.github.com>
2021-02-22 15:40:08 +0100

    Add files via upload

---
+++ SpofCode.bat
@@ -0,0 +1,78 @@
+ÿþ
+@echo off
+rem
+type help.py
+title SpofCode by Moha
+type help.py
+color 04
+mode con lines=12 cols=70
+setlocal ENABLEDELAYEDEXPANSION
+set webclient=webclient
+del "%temp%\%webclient%.vbs" /f /q /s >nul
+del "%temp%\response.txt" /f /q /s >nul
+:spofcode
+color 07
+cls
+echo.
+echo                       By SpofCode Moha
+echo.
+set ip=127.0.0.1
+set /p ip=SpofCode : 
+echo sUrl = "http://ipinfo.io/%ip%/json" > %temp%\%webclient%.vbs
+:localip
+cls
+echo set oHTTP = CreateObject("MSXML2.ServerXMLHTTP.6.0") >> %temp%\%webclient%.vbs
+echo oHTTP.open "GET", sUrl,false >> %temp%\%webclient%.vbs
+echo oHTTP.setRequestHeader "Content-Type", "application/x-www-form-urlencoded" >> %temp%\%webclient%.vbs
+echo oHTTP.setRequestHeader "Content-Length", Len(sRequest) >> %temp%\%webclient%.vbs
+echo oHTTP.send sRequest >> %temp%\%webclient%.vbs
+echo HTTPGET = oHTTP.responseText >> %temp%\%webclient%.vbs
+echo strDirectory = "%temp%\response.txt" >> %temp%\%webclient%.vbs
+echo set objFSO = CreateObject("Scripting.FileSystemObject") >> %temp%\%webclient%.vbs
+echo set objFile = objFSO.CreateTextFile(strDirectory) >> %temp%\%webclient%.vbs
+echo objFile.Write(HTTPGET) >> %temp%\%webclient%.vbs
+echo objFile.Close >> %temp%\%webclient%.vbs
+rem echo Msgbox HTTPGET,vbSystemModal+vbOKOnly+vbInformation, "IP Info" >> %temp%\%webclient%.vbs
+echo Wscript.Quit >> %temp%\%webclient%.vbs
+start %temp%\%webclient%.vbs
+set /a requests=0
+echo.
+rem echo Waiting for API response. . .
+echo  Chargement ! ( SpofCode )
+:checkresponseexists
+set /a requests=%requests% + 1
+if %requests% gtr 7 goto failed
+IF EXIST "%temp%\response.txt" (
+goto response_exist
+) ELSE (
+ping 127.0.0.1 -n 2 -w 1000 >nul
+goto checkresponseexists
+)
+:failed
+taskkill /f /im wscript.exe >nul
+del "%temp%\%webclient%.vbs" /f /q /s >nul
+echo.
+echo.
+pause
+goto menu
+:response_exist
+cls
+echo.
+for /f "delims= 	" %%i in ('findstr /i "ip city region country postal" %temp%\response.txt') do (
+	set data=%%i
+	set data=!data:,=!
+	set data=!data:"=!
+	set data=!data:ip:=IP :		!
+	set data=!data:city:=Ville :		!
+	set data=!data:region:=Etat :	!
+	set data=!data:country:=Pays :	!
+	set data=!data:postal:=Code Postal :	!
+	echo !data!
+)
+echo.
+del "%temp%\%webclient%.vbs" /f /q /s >nul
+del "%temp%\response.txt" /f /q /s >nul
+pause
+if '%ip%'=='' goto menu
+goto spofcode
+exit
